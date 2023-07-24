645195443c0b298bfcb05aec5f05e39800975489
Ryan Davis <ry.davis@f5.com>
2023-07-24 11:30:02 -0400

    use the devtools image for reference-converter (#120)
    * use the devtools image for reference-converter
    
    - follow the `.run` patten from other repos, so make targets run from
      within the devcontainer don't use docker, but outside does
    - uses a different image from devcontainer to add a bunch of vscode
      specific stuff not needed at CI time
    - runs as the `node` user to resolve a bunch of permissions issues
    - use the devtools image in the pipeline
    
    * fix CI permissions issues
    
    CI is running as `runner` when it clones the repo, so when we volume
    mount there's not enough access for the node user.
    
    * more permissions / arch shenanigans
    
    * drop debugging noise

--- tools/reference-converter/test.sh
+++ tools/reference-converter/test.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-go test -v -count 1 -race ./... 2>&1 | go-junit-report -iocopy -set-exit-code -out "$1"
+mkdir -p $(dirname "$1")
+CGO_ENABLED=1 GOOS=linux go test -v -count 1 -race ./... 2>&1 | go-junit-report -iocopy -set-exit-code -out "$1"
