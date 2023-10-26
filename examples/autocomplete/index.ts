b5d4b3f95b7339e8f25d53ce67e73c5bd3a36867
Ryan Davis <ry.davis@f5.com>
2023-10-26 12:39:26 -0400

    add autocomplete example (#67)
    demos creating updated datasets compatible with https://github.com/jaywcjlove/nginx-editor.
    
    Had to set the reference-lib package as a "module" to avoid `import` errors like:
    
    > Named export 'Format' not found. The requested module '@nginxinc/reference-lib' is a CommonJS module, which may not support all module.exports as named exports

---
+++ examples/autocomplete/index.ts
@@ -0,0 +1,36 @@
+import { getDirectives, Format, Directive } from '@nginxinc/reference-lib'
+type autocomplete = {
+  /** name of the NGINX module */
+  m: string
+  /** name */
+  n: string
+  /** markdown-formatted description */
+  d: string
+  /** default value, as an unformatted string as if a human typed it into an
+   * nginx config */
+  v?: string
+  /** markdown CSV for valid contexts */
+  c: string
+  /** markdown-formatted syntax specifications, including directive name.
+   * Multiple syntaxes are seperated by newlines */
+  s: string
+}
+
+function toAutocomplete(d: Directive): autocomplete {
+  const ret: autocomplete = {
+    m: d.module,
+    n: d.name,
+    d: d.description,
+    c: d.contexts.map((c) => '`' + c + '`').join(', '),
+    s: d.syntax.map((s) => `**${d.name}** ${s};`).join('\n'),
+  }
+
+  if (d.default) {
+    ret.v = `${d.name} ${d.default};`
+  }
+
+  return ret
+}
+
+const formatted = getDirectives(Format.Markdown).map(toAutocomplete)
+console.log(JSON.stringify(formatted, undefined, 4))
--- reference-lib/jest.config.js
+++ reference-lib/jest.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
-    testEnvironment: 'node',
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    moduleNameMapper: {
-      "reference.json": "./src/__mocks__/reference_mock.json"
-    }
-  };
+export default {
+  transform: { '^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    'reference.json': './src/__mocks__/reference_mock.json',
+  },
+}
